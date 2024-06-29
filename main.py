3434e9b63af9da2e4adc8675f8efb53d234ba5e4
Abhishek149-web <121399364+Abhishek149-web@users.noreply.github.com>
2024-06-29 12:18:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+
+    prompt_template = """
+    
+    You are a Legal expert who is a professional Lawyer with vast experience and knowledge in the Legal Matters. You will understand
+    and comprehend all the documents or the case files or the judgements that will be uploaded and then you will answer all the users query in a very efficient manner. 
+    You have all the knowledge about the process and all the terminologies related to any Legalilties and Law and order and hence give a nice detailed and easy to
+    understand answers to all the user's query. Answer the question as detailed as possible from the provided context, you can reframe the answer 
+    to make it better if there is a need to do but make sure to provide all the details, if the answer is not in
+    provided context just say, "provide some more details to get the better answer ", don't provide the wrong answer. Make sure you present the answer in proper good easy to understand and read format.\n\n
+    Context:\n {context} ?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro",temperature=0.5)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["from the given document give me the details about: ", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization = True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Karma AI Comprehension")
+    st.header("Karma AI Comprehension")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
